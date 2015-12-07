7d7dceb9aec662acb6157f9228633187f7c62d02
mihikasood <mihika.sood@ashoka.edu.in>
2015-12-08 03:26:48 +0530

    Update trial.php

--- trial.php
+++ trial.php
@@ -113,7 +113,7 @@ session_start();
  					    <li><a href="#">Address</a></li> 
  					  </ul>
  				</li>
- 				<li><form action="signout.php" method="post"><input type="submit" class="btn btn-primary" value="Sign out" style="display:inline-block;position:relative;float:right;"></form></li>
+ 				<li><form action="signout.php" method="post"><input type="submit" value="Sign out" style="display:inline-block;position:relative;float:right;"></form></li>
  			</ul>
  		</div>
  	</div>
